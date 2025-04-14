62176aef6de23c718d967492ebe1f6e799b38b82
ittou-shura <mukherjeerishiraj8@gmail.com>
2025-04-14 19:54:03 +0530

    latest

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -5,13 +5,14 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
-// const io = new Server(server, {
-//   cors: {
-//     origin:"https://chatty-beta-snowy.vercel.app",  
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],  
-//   credentials:true,
-//   },
-// });
+const io = new Server(server, {
+  cors: {
+    origin:"https://chatty-beta-snowy.vercel.app",  
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials:true,
+  },
+});
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
