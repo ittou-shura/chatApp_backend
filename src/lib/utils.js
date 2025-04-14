8dff24e768a541346b390d27c290bac37af522de
ittou-shura <mukherjeerishiraj8@gmail.com>
2025-04-14 20:49:32 +0530

    latest

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,7 +6,9 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
   });
 
   return token;
